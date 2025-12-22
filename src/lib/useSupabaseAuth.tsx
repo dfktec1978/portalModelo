5ca268ce9a19bb013ae69c5f80a9a7a6557fa958
dfktec1978 <dfk.tec.1978@gmail.com>
2025-12-21 23:50:11 -0300

    fix: corrigir imports restantes do supabaseClient

--- src/lib/useSupabaseAuth.tsx
+++ src/lib/useSupabaseAuth.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-//npm run buildimport { supabase } from "./supabase";
+npm run buildimport { supabase } from "./supabase";
 
 type SupabaseUser = {
   id: string;
